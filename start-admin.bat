0534cde6f26718d7ce51a482fd99099f0a740fe9
H5aN9X1nY56 <3186986601@qq.com>
2026-07-21 02:27:39 +0800

    make admin helper easier to open

--- start-admin.bat
+++ start-admin.bat
@@ -1,4 +1,8 @@
 @echo off
 cd /d "%~dp0"
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+  "$url='http://127.0.0.1:5177/admin.html';" ^
+  "try { Invoke-WebRequest -UseBasicParsing -Uri $url -TimeoutSec 2 | Out-Null; Start-Process $url; exit 0 } catch { }"
+if %ERRORLEVEL% EQU 0 exit /b 0
 node admin-server.js
 pause
